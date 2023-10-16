c4de858c91d4dc9d8922c7b21fdc5ff3aaeb4d6c
joel <a011777@tec.mx>
2023-10-16 16:21:02 -0600

    Initial Commit

---
+++ medTracker/medTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  medTrackerApp.swift
+//  medTracker
+//
+//  Created by Alumno on 16/10/23.
+//
+
+import SwiftUI
+
+@main
+struct medTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
