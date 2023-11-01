5fff96e322a990e03ac0fafa512de3914a8ce9ac
Sebastián Presno <sebastiandash@icloud.com>
2023-11-01 15:44:53 -0600

    Primer Commit

---
+++ medTracker/RegistroDatos1.swift
@@ -0,0 +1,18 @@
+//
+//  RegistroDatos1.swift
+//  medTracker
+//
+//  Created by Sebastian Presno Alvarado on 18/10/23.
+//
+
+import SwiftUI
+
+struct RegistroDatos1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistroDatos1()
+}
