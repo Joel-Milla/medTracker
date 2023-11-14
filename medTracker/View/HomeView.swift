9a9c2fbfb070dc8f09401af4b315232b10922283
Sebastián Presno <sebastiandash@icloud.com>
2023-11-14 12:31:18 -0600

    Coneccion entre HomeView y RegisterSymptomView

--- medTracker/View/HomeView.swift
+++ medTracker/View/HomeView.swift
@@ -9,12 +9,10 @@ import SwiftUI
 
 struct HomeView: View {
     @State var muestraEditarSintomas = false
-    var listaDatos = [Symptom(id: 1, nombre: "Peso", description: "Este es un ejemplo de descripción que es bastante largo y se va haciendo mucho más largo para comprobar la funcionalidad.", cuantitativo: true, unidades: "kg", activo: true, color: ""),
-                      Symptom(id: 2, nombre: "Cansancio", description: "Este es un ejemplo de descripción corto.", cuantitativo: false, unidades: "", activo: false, color: ""),
-                      Symptom(id: 3, nombre: "Insomnio", description: "Este es un ejemplo de descripción mediano, es decir, con esto está bien.", cuantitativo: true, unidades: "", activo: false, color: ""),
-                      Symptom(id: 4, nombre: "Estado cardíaco", description: "Latidos por minuto.", cuantitativo: true, unidades: "BPM", activo: false, color: "")]
+    @StateObject var listaDatos = SymptomList()
     
     var body: some View {
+        NavigationView {
         VStack {
             HStack {
                 Text("Síntomas")
@@ -33,11 +31,10 @@ struct HomeView: View {
                 }
                 .padding()
             }
-            NavigationView {
                 List{
-                    ForEach(listaDatos, id: \.self){ dato in
+                    ForEach($listaDatos.symptoms, id: \.self){ $dato in
                         NavigationLink{
-                            //VistaRegistrar(undato: dato)
+                            RegisterSymptomView(symptom: $dato)
                         } label: {
                             Celda(unDato : dato)
                         }
