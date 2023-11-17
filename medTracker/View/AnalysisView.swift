ef679f53b9003f2735dd5529bccc302d3f0212f1
diegovelsal <A01177877@itesm.mx>
2023-11-17 14:53:25 -0600

    structs funcionando y preparados para JSON y base de datos

--- medTracker/View/AnalysisView.swift
+++ medTracker/View/AnalysisView.swift
@@ -9,89 +9,15 @@ import SwiftUI
 import Charts
 
 struct AnalysisView: View {
-   @State var registers = [
-        Register(idSymptom: 1, fecha: Date.now, cantidad: 80, notas: "Esto es una nota."),
-        Register(idSymptom: 1, fecha: Date.now.addingTimeInterval(86400), cantidad: 80.5, notas: "Esto es una nota."),
-        Register(idSymptom: 1, fecha: Date.now.addingTimeInterval(86400*2), cantidad: 80.2, notas: "Esto es una nota."),
-        Register(idSymptom: 1, fecha: Date.now.addingTimeInterval(86400*3), cantidad: 20, notas: "Esto es una nota."),
-        
-        Register(idSymptom: 2, fecha: Date.now, cantidad: 80, notas: "Esto es una nota."),
-        
-        Register(idSymptom: 3, fecha: Date.now, cantidad: 80, notas: "Esto es una nota."),
-        Register(idSymptom: 3, fecha: Date.now.addingTimeInterval(86400), cantidad: 70, notas: "Esto es una nota."),
-        Register(idSymptom: 3, fecha: Date.now.addingTimeInterval(86400*2), cantidad: 30, notas: "Esto es una nota."),
-        Register(idSymptom: 3, fecha: Date.now.addingTimeInterval(86400*3), cantidad: 40, notas: "Esto es una nota."),
-        
-        Register(idSymptom: 4, fecha: Date.now, cantidad: 80, notas: "Esto es una nota.")
-    ]
     
-    @State var symptoms = [
-        Symptom(id: 1, nombre: "Peso", description: "Este es un ejemplo de descripción que es bastante largo y se va haciendo mucho más largo para comprobar la funcionalidad.", cuantitativo: true, unidades: "kg", activo: true, color: "#007AF"),
-        Symptom(id: 2, nombre: "Cansancio", description: "Este es un ejemplo de descripción corto.", cuantitativo: false, unidades: "", activo: false, color: "#AF43EB"),
-        Symptom(id: 3, nombre: "Insomnio", description: "Este es un ejemplo de descripción mediano, es decir, con esto está bien.", cuantitativo: true, unidades: "", activo: false, color: "#D03A20"),
-        Symptom(id: 4, nombre: "Estado cardíaco", description: "Latidos por minuto.", cuantitativo: true, unidades: "BPM", activo: false, color: "#86B953")
-    ]
+    @StateObject var listSymp = SymptomList()
+    @StateObject var registers = RegisterList()
     
     var body: some View {
         VStack {
             TabView {
-                ForEach(symptoms, id: \.self) { symptom in
-                    VStack(alignment: .leading) {
-                        Text(symptom.nombre)
-                            .foregroundColor(Color(hex: symptom.color))
-                            .font(.largeTitle)
-                            .bold()
-                            .padding(.top, 30)
-                        
-                        Text("Descripción: ")
-                            .font(.system(size: 24))
-                            .padding(.top, 22)
-                        
-                        @State var descripcion = symptom.description
-                        
-                        ScrollView {
-                            TextField("", text: $descripcion, axis : .vertical)
-                                .textFieldStyle(.roundedBorder)
-                                .font(.system(size: 18))
-                                .lineSpacing(4)
-                                .background(Color.white)
-                                .border(.black)
-                                .padding(.trailing, 20)
-                                .disabled(true)
-                        }
-                        
-                        Text("Últimos registros: ")
-                            .font(.system(size: 24))
-                            .padding(.top, 30)
-                        
-                        Chart {
-                            ForEach(registers.filter { $0.idSymptom == symptom.id
-                            }, id:\.self) { register in
-                                BarMark(x: .value("DIA", register.fecha.formatted(.dateTime.day().month())),
-                                        y: .value("CANTIDAD", register.cantidad))
-                                    .cornerRadius(10)
-                                    .annotation{
-                                        Text(String(format: "%.0f", register.cantidad))
-                                    }
-                            }
-                        }
-                        .frame(height: 300)
-                        .foregroundStyle(Color(hex: symptom.color))
-                        .padding()
-                        .shadow(radius: /*@START_MENU_TOKEN@*/6/*@END_MENU_TOKEN@*/)
-                        .chartXAxisLabel("DIA", alignment: .topTrailing, spacing: 10)
-                        .chartYAxisLabel("CANTIDAD", spacing: 10)
-                        .foregroundColor(.black)
-                        .chartPlotStyle { plotContent in
-                            plotContent
-                                .background(.gray.opacity(0.1))
-                                .border(Color.black, width: 2)
-                        }
-                        Spacer(minLength: 20)
-                    }
-                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
-                    .padding(.leading, 20)
-                    
+                ForEach(listSymp.symptoms, id: \.id) { symptom in
+                    AnalysisItemView(symptom: symptom, registers: registers)
                 }
             }
             .tabViewStyle(.page)
@@ -99,14 +25,82 @@ struct AnalysisView: View {
             
             Spacer(minLength: 50)
         }
-        .background(Color("mainWhite"))
     }
 }
+
+struct AnalysisItemView: View {
+    let symptom: Symptom
+    let registers: RegisterList
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(symptom.nombre)
+                .foregroundColor(Color(hex: symptom.color))
+                .font(.largeTitle)
+                .bold()
+                .padding(.top, 30)
+            
+            Text("Descripción: ")
+                .font(.system(size: 24))
+                .padding(.vertical, 10)
+            
+            Text("\(symptom.description)")
+                .padding(.trailing, 20)
+                .foregroundColor(Color(hex: symptom.color))
+                .lineSpacing(4)
+                .font(.system(size: 18))
+                .frame(height: 120, alignment: .top)
+    
+            /*@State var descripcion = symptom.description
+             
+             ScrollView {
+                 TextField("", text: $descripcion, axis : .vertical)
+                     .textFieldStyle(.roundedBorder)
+                     .font(.system(size: 18))
+                     .lineSpacing(4)
+                     .background(Color.white)
+                     .border(.black)
+                     .padding(.trailing, 20)
+                     .disabled(true)
+             }*/
+            
+            Text("Últimos registros:")
+                .font(.system(size: 24))
+                .padding(.top, 30)
+            
+            Chart {
+                ForEach(registers.registers.filter { $0.idSymptom == symptom.id }, id:\.self) { register in
+                    BarMark(x: .value("DIA", register.fecha.formatted(.dateTime.day().month())), y: .value("CANTIDAD", register.cantidad))
+                        .cornerRadius(10)
+                        .annotation {
+                            Text(String(format: "%.0f", register.cantidad))
+                        }
+                }
+            }
+            .frame(height: 300)
+            .foregroundStyle(Color(hex: symptom.color))
+            .padding()
+            .shadow(radius: /*@START_MENU_TOKEN@*/6/*@END_MENU_TOKEN@*/)
+            .chartXAxisLabel("DIA", alignment: .topTrailing, spacing: 10)
+            .chartYAxisLabel("CANTIDAD", spacing: 10)
+            .foregroundColor(.black)
+            .chartPlotStyle { plotContent in
+                plotContent
+                    .background(.gray.opacity(0.1))
+                    .border(Color.black, width: 2)
+            }
+            
+            Spacer(minLength: 20)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+        .padding(.leading, 20)
+    }
+}
+
+
 
 struct analysis_Previews: PreviewProvider {
     static var previews: some View {
         AnalysisView()
-            .environmentObject(SymptomList())
-            .environmentObject(RegisterList())
     }
 }
--- medTracker/ViewModel/SymptomList.swift
+++ medTracker/ViewModel/SymptomList.swift
@@ -8,19 +8,41 @@
 import Foundation
 
 class SymptomList : ObservableObject {
-    @Published var symptoms = [Symptom]()
+    @Published var symptoms = [Symptom]() {
+        didSet {
+            if let codificado = try? JSONEncoder().encode(symptoms) {
+                try? codificado.write(to: rutaArchivos())
+            }
+        }
+    }
+    
+    func rutaArchivos() -> URL {
+        let url = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+        let pathArchivo = url.appendingPathComponent("Symptoms.JSON")
+        return pathArchivo
+    }
     
     init() {
-        symptoms.append(Symptom(id: 1, nombre: "Peso", description: "Este es un ejemplo de descripción que es bastante largo y se va haciendo mucho más largo para comprobar la funcionalidad.", cuantitativo: true, unidades: "kg", activo: true, color: ""))
-        symptoms.append(Symptom(id: 2, nombre: "Cansancio", description: "Este es un ejemplo de descripción corto.", cuantitativo: false, unidades: "", activo: false, color: ""))
-        symptoms.append(Symptom(id: 3, nombre: "Insomnio", description: "Este es un ejemplo de descripción mediano, es decir, con esto está bien.", cuantitativo: true, unidades: "", activo: false, color: ""))
-        symptoms.append(Symptom(id: 4, nombre: "Estado cardíaco", description: "Latidos por minuto.", cuantitativo: true, unidades: "BPM", activo: false, color: ""))
+        if let datosRecuperados = try? Data.init(contentsOf: rutaArchivos()) {
+            if let datosDecodificados = try? JSONDecoder().decode([Symptom].self, from: datosRecuperados) {
+                symptoms = datosDecodificados
+                if symptoms == [] {
+                    symptoms = [Symptom(id: 1, nombre: "Peso", description: "Este es un ejemplo de descripción que es bastante largo y se va haciendo mucho más largo para comprobar la funcionalidad.", cuantitativo: true, unidades: "kg", activo: true, color: "#007AF"),
+                    Symptom(id: 2, nombre: "Cansancio", description: "Este es un ejemplo de descripción corto.", cuantitativo: false, unidades: "", activo: false, color: "#AF43EB"),
+                    Symptom(id: 3, nombre: "Insomnio", description: "Este es un ejemplo de descripción mediano, es decir, con esto está bien.", cuantitativo: true, unidades: "", activo: false, color: "#D03A20"),
+                    Symptom(id: 4, nombre: "Estado cardíaco", description: "Latidos por minuto.", cuantitativo: true, unidades: "BPM", activo: false, color: "#86B953"),
+                    Symptom(id: 5, nombre: "Estado cardíaco 2", description: "Latidos por minuto.", cuantitativo: true, unidades: "BPM", activo: false, color: "#86B953")]
+                }
+                return
+            }
+        }
+        symptoms = []
     }
 }
 
 /*
- Symptom(id: 1, nombre: "Peso", description: "Este es un ejemplo de descripción que es bastante largo y se va haciendo mucho más largo para comprobar la funcionalidad.", cuantitativo: true, unidades: "kg", activo: true, color: ""),
- Symptom(id: 2, nombre: "Cansancio", description: "Este es un ejemplo de descripción corto.", cuantitativo: false, unidades: "", activo: false, color: ""),
- Symptom(id: 3, nombre: "Insomnio", description: "Este es un ejemplo de descripción mediano, es decir, con esto está bien.", cuantitativo: true, unidades: "", activo: false, color: ""),
- Symptom(id: 4, nombre: "Estado cardíaco", description: "Latidos por minuto.", cuantitativo: true, unidades: "BPM", activo: false, color: "")
+ Symptom(id: 1, nombre: "Peso", description: "Este es un ejemplo de descripción que es bastante largo y se va haciendo mucho más largo para comprobar la funcionalidad.", cuantitativo: true, unidades: "kg", activo: true, color: "#007AF"),
+ Symptom(id: 2, nombre: "Cansancio", description: "Este es un ejemplo de descripción corto.", cuantitativo: false, unidades: "", activo: false, color: "#AF43EB"),
+ Symptom(id: 3, nombre: "Insomnio", description: "Este es un ejemplo de descripción mediano, es decir, con esto está bien.", cuantitativo: true, unidades: "", activo: false, color: "#D03A20"),
+ Symptom(id: 4, nombre: "Estado cardíaco", description: "Latidos por minuto.", cuantitativo: true, unidades: "BPM", activo: false, color: "#86B953")
  */
