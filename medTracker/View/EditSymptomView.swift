bfad9b36491bcfa5fb3b0555eaaa798ad37e9876
Joel-Milla <jalejandro.milla@outlook.com>
2023-11-20 13:02:24 -0600

    Add view saying that there is no symptom to register in editSymptomView.

--- medTracker/View/EditSymptomView.swift
+++ medTracker/View/EditSymptomView.swift
@@ -12,42 +12,53 @@ struct EditSymptomView: View {
     @State var muestraNewSymptom = false
     @Environment(\.presentationMode) var presentationMode
     @ObservedObject var listaDatos: SymptomList
-
+    
     var body: some View {
         NavigationView {
             VStack {
-                List {
-                    Section(header: Text("Lista de datos de salud")) {
-                        ForEach(listaDatos.symptoms.indices, id: \.self) { index in
-                            Toggle(listaDatos.symptoms[index].nombre, isOn: $listaDatos.symptoms[index].activo)
+                if listaDatos.symptoms.isEmpty {
+                    EmptyListView(
+                        title: "No hay sintomas registrados",
+                        message: "Porfavor de agregar sintomas para poder empezar a registrar."
+                    )
+                }
+                else {
+                    List {
+                        Section(header: Text("Lista de datos de salud")) {
+                            ForEach(listaDatos.symptoms.indices, id: \.self) { index in
+                                Toggle(listaDatos.symptoms[index].nombre, isOn: $listaDatos.symptoms[index].activo)
+                            }
                         }
                     }
+                    .id(refreshID)
+                    .background(Color("mainGray"))
+                    .font(.title3)
+                .navigationTitle("Edita tus datos de salud")
                 }
-                .id(refreshID)
-                .background(Color("mainGray"))
-                .font(.title3)
-                .navigationBarItems(leading: Button(action: {
-                    presentationMode.wrappedValue.dismiss()
-                }) {
-                    Image(systemName: "arrow.left")
-                    Text("Regresar")
-                })
-                .navigationBarItems(trailing:
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: {
+                        presentationMode.wrappedValue.dismiss()
+                    }, label: {
+                        Image(systemName: "arrow.left")
+                        Text("Regresar")
+                    })
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
                     Button {
                         muestraNewSymptom = true
                     } label: {
                         Image(systemName: "plus")
                     }
-                )
-                .sheet(isPresented: $muestraNewSymptom) {
-                    AddSymptomView(symptoms: listaDatos, createAction: listaDatos.makeCreateAction())
-                        .onChange(of: listaDatos.symptoms) { _ in
-                            refreshID = UUID()
-                        }
                 }
-                .navigationTitle("Edita tus datos de salud")
             }
-            .background(Color("mainGray"))
+            .sheet(isPresented: $muestraNewSymptom) {
+                AddSymptomView(symptoms: listaDatos, createAction: listaDatos.makeCreateAction())
+                    .onChange(of: listaDatos.symptoms) { _ in
+                        refreshID = UUID()
+                    }
+            }
         }
     }
 }
