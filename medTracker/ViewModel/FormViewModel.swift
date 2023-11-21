07225412d5a662ea3b42023a24a055fe591989e6
Joel-Milla <jalejandro.milla@outlook.com>
2023-11-20 18:50:23 -0600

    Add comments to fomrviewmodel and organize information.

--- medTracker/ViewModel/FormViewModel.swift
+++ medTracker/ViewModel/FormViewModel.swift
@@ -7,38 +7,51 @@
 
 import Foundation
 
+/**********************
+ This class contains all the data that helps to create a new user.
+ **********************************/
+
 @MainActor
 @dynamicMemberLookup
 class FormViewModel<Value>: ObservableObject {
-    typealias Action = (Value) async throws -> Void
-    
     @Published var value: Value
     @Published var error: Error?
+    @Published var state: State = .idle // state of the request.
     
-    @Published var state: State = .idle
-    enum State {
-        case idle
-        case isLoading
-    }
-
-    subscript<T>(dynamicMember keyPath: WritableKeyPath<Value, T>) -> T {
-        get { value[keyPath: keyPath] }
-        set { value[keyPath: keyPath] = newValue }
-    }
+    typealias Action = (Value) async throws -> Void
+    private let action: Action //closure of type action.
     
-    private let action: Action
-    
+    /**********************
+     Important initialization methods
+     **********************************/
     init(initialValue: Value, action: @escaping Action) {
         self.value = initialValue
         self.action = action
     }
     
+    enum State {
+        case idle
+        case isLoading
+    }
+    
+    /**********************
+     Helper functions
+     **********************************/
+    
+    // Helper function to create request to firebase to create user.
+    subscript<T>(dynamicMember keyPath: WritableKeyPath<Value, T>) -> T {
+        get { value[keyPath: keyPath] }
+        set { value[keyPath: keyPath] = newValue }
+    }
+    
+    // Function that makes the request to database to create user.
     nonisolated func submit() {
         Task {
             await handleSubmit()
         }
     }
     
+    // function that handles the request and catches the error.
     private func handleSubmit() async {
         state = .isLoading
         do {
