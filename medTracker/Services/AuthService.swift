947a2b159c93f4c88e97e143f57d25378946fbce
Joel-Milla <jalejandro.milla@outlook.com>
2023-11-20 19:09:12 -0600

    Add comments to autService and organize data.

--- medTracker/Services/AuthService.swift
+++ medTracker/Services/AuthService.swift
@@ -9,7 +9,9 @@ import Foundation
 import FirebaseAuth
 
 
-
+/**********************
+ This class contains helper functions to authenticate the user (the log in view and register a new user)
+ **********************************/
 @MainActor
 class AuthService: ObservableObject {
     @Published var isAuthenticated = false
@@ -17,45 +19,45 @@ class AuthService: ObservableObject {
     private let auth = Auth.auth()
     private var listener: AuthStateDidChangeListenerHandle?
     
+    /**********************
+     Important initialization methods
+     **********************************/
     init() {
         listener = auth.addStateDidChangeListener { [weak self] _, user in
             self?.isAuthenticated = user != nil
         }
     }
     
+    /**********************
+     Helper functions
+     **********************************/
+    
+    // Function to create an account based on a name, email, and password.
     func createAccount(name: String, email: String, password: String) async throws {
         do {
             let result = try await auth.createUser(withEmail: email, password: password)
-                    try await result.user.updateProfile(\.displayName, to: name)
-            AuthService.writeEmail(email)
+            try await result.user.updateProfile(\.displayName, to: name)
+            //AuthService.writeEmail(email)
+            HelperFunctions.write(email, inPath: "email.JSON")
         } catch {
             throw error
         }
         
     }
     
-    static func rutaArchivos() -> URL {
-        let url = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
-        let pathArchivo = url.appendingPathComponent("email.JSON")
-        return pathArchivo
-    }
-    
-    static func writeEmail(_ value: String) {
-        if let codificado = try? JSONEncoder().encode(value) {
-            try? codificado.write(to: rutaArchivos())
-        }
-    }
-    
+    // Function that tries to sign in.
     func signIn(email: String, password: String) async throws {
         try await auth.signIn(withEmail: email, password: password)
     }
     
+    // Function to sign out.
     func signOut() throws {
         try auth.signOut()
     }
-
+    
 }
 
+// Function to update the profile
 private extension FirebaseAuth.User {
     func updateProfile<T>(_ keyPath: WritableKeyPath<UserProfileChangeRequest, T>, to newValue: T) async throws {
         var profileChangeRequest = createProfileChangeRequest()
