dc5fb4394f850d54db74b98c010cb68f034d8dad
Sebastián Presno <sebastiandash@icloud.com>
2023-11-22 22:09:17 -0600

    arregle HomeView(no sale el logo en home) pero si los botones y tambien ya exporta por nombre de sintoma

--- medTracker/View/HomeView.swift
+++ medTracker/View/HomeView.swift
@@ -16,103 +16,108 @@ struct HomeView: View {
     @State private var muestraEditarSintomas = false
     @State private var muestraAgregarSintomas = false
     @State private var muestraNewSymptom = false
+    @State private var muestraShare = false
     @State private var isHomeViewActive = true
     //@State private var refreshID = UUID()
     
     
     var body: some View {
-        NavigationStack {
-            VStack {
-                // Show the view based on symptomList state (loading, emptyArray, arrayWithValues).
-                switch listaDatos.state {
-                case .isLoading:
-                    ProgressView() //Loading animation
-                case .isEmpty:
-                    //Calls a view to show that the symptom list is empty
-                    //The action serves as a button to send the user to a page to create a symptom.
-                    EmptyListView(
-                        title: "No hay sintomas registrados",
-                        message: "Porfavor de agregar sintomas para poder empezar a registrar.",
-                        nameButton: "Agregar Sintoma"
-                        //action: { muestraNewSymptom = true }
-                    )
-                    // The sheets sends the user to the view to create a new symptom.
-                    .sheet(isPresented: $muestraNewSymptom) {
-                        AddSymptomView(symptoms: listaDatos, createAction: listaDatos.makeCreateAction())
-                    }
-                case .complete:
-                    List{
-                        ForEach(listaDatos.symptoms.indices, id: \.self) { index in
-                            if listaDatos.symptoms[index].activo {
-                                let symptom = listaDatos.symptoms[index]
-                                NavigationLink{
-                                    RegisterSymptomView(symptom: $listaDatos.symptoms[index], registers: registers, sliderValue : .constant(0.155) ,createAction: registers.makeCreateAction())
-                                } label: {
-                                    Celda(unDato : symptom)
+        ZStack{
+            NavigationStack {
+                VStack {
+                    // Show the view based on symptomList state (loading, emptyArray, arrayWithValues).
+                    switch listaDatos.state {
+                    case .isLoading:
+                        ProgressView() //Loading animation
+                    case .isEmpty:
+                        //Calls a view to show that the symptom list is empty
+                        //The action serves as a button to send the user to a page to create a symptom.
+                        EmptyListView(
+                            title: "No hay sintomas registrados",
+                            message: "Porfavor de agregar sintomas para poder empezar a registrar.",
+                            nameButton: "Agregar Sintoma"
+                            //action: { muestraNewSymptom = true }
+                        )
+                        // The sheets sends the user to the view to create a new symptom.
+                        .sheet(isPresented: $muestraNewSymptom) {
+                            AddSymptomView(symptoms: listaDatos, createAction: listaDatos.makeCreateAction())
+                        }
+                    case .complete:
+                        List{
+                            ForEach(listaDatos.symptoms.indices, id: \.self) { index in
+                                if listaDatos.symptoms[index].activo {
+                                    let symptom = listaDatos.symptoms[index]
+                                    NavigationLink{
+                                        RegisterSymptomView(symptom: $listaDatos.symptoms[index], registers: registers, sliderValue : .constant(0.155) ,createAction: registers.makeCreateAction())
+                                    } label: {
+                                        Celda(unDato : symptom)
+                                    }
+                                    .padding(10)
+                                    //                                .padding(10)
+                                    
                                 }
-                                .padding(10)
-                                //                                .padding(10)
-                                
                             }
                         }
                     }
-                }
-                
-            }
-            .navigationTitle("Datos de salud")
-            .toolbar {
-                // Button to traverse to EditSymptomView.
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button {
-                        muestraEditarSintomas = true
-                    } label: {
-                        Image(systemName: "square.and.arrow.up")
-                    }
-                }
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button {
-                        muestraEditarSintomas = true
-                    } label: {
-                        Text("Editar")
-                    }
                     
                 }
+                .navigationTitle("Datos de salud")
+                .toolbar {
+                    // Button to traverse to EditSymptomView.
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button {
+                            muestraShare = true
+                        } label: {
+                            Image(systemName: "square.and.arrow.up")
+                        }
+                    }
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button {
+                            muestraEditarSintomas = true
+                        } label: {
+                            Text("Editar")
+                        }
+                        
+                    }
+                }
+                // Present full screen the EditSymptomView.
+                .fullScreenCover(isPresented: $muestraEditarSintomas) {
+                    //ShareView(listaDatos: listaDatos, registers: registers)
+                    EditSymptomView(listaDatos: listaDatos)
+                }
+//                .onChange(of: listaDatos.state) { newState in
+//                    // Update the isHomeViewActive based on the state of listaDatos
+//                    isHomeViewActive = newState != .complete
+//                }
+                .sheet(isPresented: $muestraAgregarSintomas, content: {
+                    AddSymptomView(symptoms: listaDatos, createAction: listaDatos.makeCreateAction())
+                    /*.onChange(of: listaDatos.symptoms) { _ in
+                     refreshID = UUID()
+                     }*/
+                })
+                .sheet(isPresented: $muestraShare, content: {
+                    ShareView(listaDatos: listaDatos, registers: registers)
+                })
             }
-            // Present full screen the EditSymptomView.
-            .fullScreenCover(isPresented: $muestraEditarSintomas) {
-                //ShareView(listaDatos: listaDatos, registers: registers)
-                EditSymptomView(listaDatos: listaDatos)
-            }
-            .onChange(of: listaDatos.state) { newState in
-                // Update the isHomeViewActive based on the state of listaDatos
-                isHomeViewActive = newState != .complete
-            }
-            .background(Color("mainGray"))
-            .ignoresSafeArea()
-            
-            Image("logoP")
-                .resizable()
-                .imageScale(.small)
-                .aspectRatio(contentMode: .fit)
-                .frame(height: 50)
-                .offset(x: 0, y: -360)
-            
-            Button{
-                muestraAgregarSintomas = true
-            }label:{
-                Label("Agregar síntoma", systemImage: "square.and.pencil")
-            }
-            .buttonStyle(Button1MedTracker(backgroundColor: Color("blueGreen")))
-            .offset(x: 80, y: 280)
-        }
-        .sheet(isPresented: $muestraAgregarSintomas, content: {
-            AddSymptomView(symptoms: listaDatos, createAction: listaDatos.makeCreateAction())
-                /*.onChange(of: listaDatos.symptoms) { _ in
-                 refreshID = UUID()
-                 }*/
-            })
+            .overlay(
+                Group {
+                    if isHomeViewActive {
+                        Button {
+                            muestraAgregarSintomas = true
+                        } label: {
+                            Label("Agregar nuevo dato", systemImage: "square.and.pencil")
+                        }
+                        .buttonStyle(Button1MedTracker(backgroundColor: Color("blueGreen")))
+                        .offset(x: -14, y: -75)
+                        .sheet(isPresented: $muestraAgregarSintomas) {
+                            AddSymptomView(symptoms: listaDatos, createAction: listaDatos.makeCreateAction())
+                        }
+                    }
+                },
+                alignment: .bottomTrailing)
         }
     }
+}
     
     // Struct to show the respective icon for each symptom.
     struct Celda: View {
@@ -136,4 +141,4 @@ struct HomeView: View {
             HomeView(listaDatos: SymptomList(), registers: RegisterList())
         }
     }
-}
+
