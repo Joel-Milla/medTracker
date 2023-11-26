e81e995e46afc33a06e3507a57368426dda5b866
Joel-Milla <jalejandro.milla@outlook.com>
2023-11-26 00:10:38 -0600

    Add style to the row of patients.

---
+++ medTracker/View/DoctorViews/AnalysisDoctorView.swift
@@ -0,0 +1,18 @@
+//
+//  AnalysisDoctorView.swift
+//  medTracker
+//
+//  Created by Alumno on 26/11/23.
+//
+
+import SwiftUI
+
+struct AnalysisDoctorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnalysisDoctorView()
+}
